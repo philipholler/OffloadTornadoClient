5453cfdf325b130c3dea196b267909f50f84cada
Philip Holler <philipholler94@icloud.com>
2020-09-28 14:04:00 +0200

    Can now add jobs to the list. Almost done with least viable product.

--- src/main/kotlin/dk/aau/src/controller/DashboardController.kt
+++ src/main/kotlin/dk/aau/src/controller/DashboardController.kt
@@ -1,7 +1,30 @@
 package dk.aau.src.controller
 
+import dk.aau.src.model.Job
+import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.collections.ObservableList
 import tornadofx.*
 
 class DashboardController: Controller(){
+    var jobs = observableListOf(listOf(
+            Job("Photo Analysis", "Started", 0, 5),
+            Job("World peace", "Working", 2, 1),
+            Job("Photoshop Trumps hair", "Done", 15, 15)))
+    var hostRange = (0..25).toList().asObservable()
+    var selectedHost = SimpleObjectProperty(0)
+    var textFieldValue = SimpleStringProperty("Insert path to dir")
 
+    fun uploadJob(pathToJobDir: String, numbOfHostsRequested: Int): Boolean{
+        // Check if job lives up to criteria
+        if (true){
+            runLater{
+                val name = pathToJobDir.subSequence(pathToJobDir.lastIndexOf('/') + 1, pathToJobDir.length).toString()
+                val job: Job = Job(name, "Created", 0, numbOfHostsRequested)
+                jobs.add(job)
+            }
+            return true
+        }
+        return false
+    }
 }
--- src/main/kotlin/dk/aau/src/view/DashboardScreen.kt
+++ src/main/kotlin/dk/aau/src/view/DashboardScreen.kt
@@ -1,27 +1,18 @@
 package dk.aau.src.view
 
-import dk.aau.src.app.Styles
 import dk.aau.src.controller.DashboardController
 import dk.aau.src.controller.LoginController
 import dk.aau.src.model.Job
 import dk.aau.src.model.UserModel
-import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.control.TableView.CONSTRAINED_RESIZE_POLICY
 import javafx.scene.layout.Priority
-import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class DashboardScreen : View("Offloading Dashboard"){
     val user: UserModel by inject()
     val loginController: LoginController by inject()
-    val dashboardController: DashboardController by inject()
-
-    val jobs = listOf(
-            Job("Photo Analysis", "Started", 0, 5),
-            Job("World peace", "Working", 2, 1),
-            Job("Photoshop Trumps hair", "Done", 15, 15),
-    ).asObservable()
+    val dbController: DashboardController by inject()
 
     override val root = hbox(){
         setPrefSize(1000.0, 700.0)
@@ -32,13 +23,12 @@ class DashboardScreen : View("Offloading Dashboard"){
             alignment = Pos.TOP_LEFT
             paddingAll = 10.0
 
-            tableview(jobs){
+            tableview(dbController.jobs){
                 minWidth = 500.0
                 columnResizePolicy = CONSTRAINED_RESIZE_POLICY
                 vboxConstraints {
                     vGrow = Priority.ALWAYS
                 }
-
                 readonlyColumn("Job", Job::name)
                 readonlyColumn("Hosts requested", Job::hostsRequested)
                 readonlyColumn("Hosts Assigned", Job::hostsAssigned)
@@ -51,9 +41,10 @@ class DashboardScreen : View("Offloading Dashboard"){
         vbox{
             paddingAll = 10.0
             fitToParentSize()
-            alignment = Pos.TOP_RIGHT
+
 
             hbox{
+                alignment = Pos.TOP_RIGHT
                 vbox{
                     paddingTop = 5
                     label ("Offload Jobs")
@@ -72,13 +63,54 @@ class DashboardScreen : View("Offloading Dashboard"){
                 maxHeight = 500.0
             }
 
-            label(user.name) {
-                style {
-                    fontWeight = FontWeight.BOLD
-                    fontSize = 24.px
+            form {
+                alignment = Pos.BOTTOM_LEFT
+                fitToParentSize()
+
+                fieldset() {
+                    fitToParentWidth()
+                    hbox {
+                        vbox {
+                            // Path to file
+                            textfield(dbController.textFieldValue)
+                            prefWidth = 400.0
+                            paddingRight = 10
+                        }
+                        hbox {
+                            field {
+                                button("Select dir") {
+                                    action {
+                                        dbController.textFieldValue.value = chooseDirectory("Select Target Directory").toString()
+                                        runAsyncWithProgress {
+                                            dbController.uploadJob(dbController.textFieldValue.value,
+                                                    dbController.selectedHost.value)
+                                        }
+                                    }
+                                }
+                                paddingRight = 10
+                                minWidth = 80.0
+                            }
+                            field {
+                                combobox(dbController.selectedHost, dbController.hostRange) {
+                                    makeAutocompletable()
+                                }
+                                minWidth = 70.0
+                                paddingRight = 10
+                            }
+                        }
+                    }
+                    hbox{
+                        maxWidth = 550.0
+                        paddingTop = 20
+                        paddingRight = 15
+                        alignment = Pos.BOTTOM_RIGHT
+                        button("Upload Job")
+                    }
                 }
             }
-
         }
     }
 }
+
+
+
